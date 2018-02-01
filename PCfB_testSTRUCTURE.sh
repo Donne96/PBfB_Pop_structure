95bc4c2b39ef5aca2d8dcda53d85ddd02839b7c1
Donne96 <35833709+Donne96@users.noreply.github.com>
2018-02-01 16:11:58 +0100

    Update PCfB_testSTRUCTURE.sh

--- PCfB_testSTRUCTURE.sh
+++ PCfB_testSTRUCTURE.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #create directory for the structure output to be stored
-mkdir -p ${HOME}/Documents/Results_WSPopstruc
+mkdir -p ${HOME}/PBfB_Pop_structure/Results_WSPopstruc
 
 #A for loop is created within a for loop to create replicates for each K value
 #3 is the number of replicates at each value of K.
 #5 is the number of K for which structure is tested.
 for k in {1..5}; do for rep in {1..10};
 do 
-structure -m Donne96/PBfB_Pop_structure/mainparams -e Donne96/PBfB_Pop_structure_extraparams -i Donne96/PBfB_Pop_structure/WS_struc_msat.txt -K ${k} -o ${HOME}/Documents/Results_WSPopstruc/outfile_k${k}_rep${rep}
+structure -m ${HOME}/PBfB_Pop_structure/mainparams -e ${HOME}/PBfB_Pop_structure_extraparams -i ${HOME}/PBfB_Pop_structure/WS_struc_msat.txt -K ${k} -o ${HOME}/PBfB_Pop_structure/Results_WSPopstruc/outfile_k${k}_rep${rep}
 done
 done
