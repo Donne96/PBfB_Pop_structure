5b6dd850047c383d6cbff24d6cc6a241a2e643d0
Donne96 <35833709+Donne96@users.noreply.github.com>
2018-02-01 16:13:43 +0100

    Update PCfB_testHW.R

--- PCfB_testHW.R
+++ PCfB_testHW.R
@@ -1,5 +1,5 @@
 rm(list = ls()) # remove global environment
-setwd("Donne96/PBfB_Pop_structure") # set working directory
+setwd("${HOME}/PBfB_Pop_structure") # set working directory
 
 # Load packages
 library(xlsx)
